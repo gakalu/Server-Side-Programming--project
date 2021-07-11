792d04286b512681d78095de30cfa3bc34af82bd
gakalu <69823298+gakalu@users.noreply.github.com>
2021-07-11 00:24:32 -0500

    user modefied

---
+++ server-side/models/user.js
@@ -0,0 +1,23 @@
+const { ObjectID } = require('mongodb');
+const getDb = require('../utils/database').getDb;
+
+
+module.exports = class User {
+    constructor(_id, username, password, role) {
+        this._id = _id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    login() {
+        return getDb().collection('users').findOne({ username: this.username, password: this.password });
+    }
+
+    signUp() {
+        getDb().collection('users').insertOne({ username: this.username, password: this.password });
+        return this;
+    }
+}
+
+//user = [new User('john', 'admin1', 'admin'), new User('bella', 'memeber', 'user')];
